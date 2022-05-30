4a365360879dec29feaf4109364d9589152cb66b
sinofseven <em.s.00001@gmail.com>
2022-05-30 14:23:35 +0900

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
     <v-row>
       <v-col>
         <h2>Tools of Atelier Of Ryza</h2>
+        <a href="https://github.com/sinofseven/tools-of-atelier-of-ryza"
+          ><h3>GitHubリポジトリ</h3></a
+        >
       </v-col>
     </v-row>
   </v-container>
